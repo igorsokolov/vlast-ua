6ce56829f22221b3aca08c8e8ca9c05eeec14eeb
Igor Sokolov <igor.sokolov@gmail.com>
2014-04-01 16:46:14 +0300

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Myapp::Application.routes.draw do
   resources :entities
 
+  root 'entities#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
