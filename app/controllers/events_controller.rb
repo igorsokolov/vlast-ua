f0e1acf77f62318b93f1fd24a2642bf495427d39
Igor Sokolov <igor.sokolov@gmail.com>
2014-04-02 18:54:45 +0300

    Fixed event params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,8 +24,8 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    year = event_params[:event].delete :event_date
-    event_params[:event][:event_date] = Date.parse("01/01/#{year}")
+    year = event_params.delete :event_date
+    event_params[:event_date] = Date.parse("01/01/#{year}")
     @event = Event.new(event_params)
 
     respond_to do |format|
@@ -42,8 +42,8 @@ class EventsController < ApplicationController
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
   def update
-    year = event_params[:event].delete :event_date
-    event_params[:event][:event_date] = Date.parse("01/01/#{year}")
+    year = event_params.delete :event_date
+    event_params[:event_date] = Date.parse("01/01/#{year}")
 
     respond_to do |format|
       if @event.update(event_params)
